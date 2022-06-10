f8c7914cda8b2465cee41349ccd3346543f208c8
benjamin <35360507+foghill@users.noreply.github.com>
2022-06-10 14:00:59 -0400

    Meme Component and css styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 import Header from "./Header";
+import Meme from "./Meme";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   );
 }
