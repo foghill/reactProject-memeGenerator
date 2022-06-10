727e5f9132d8f0cc42e53a0f895e143cbc7da608
benjamin <35360507+foghill@users.noreply.github.com>
2022-06-10 13:32:27 -0400

    Header updates

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header>
-      <h1>I am a header</h1>
+    <header className="header">
+      <img src="./trollface.png" alt="" className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
     </header>
   );
 }
